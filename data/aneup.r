3bf8e47ecb173038718b2a496587f653f7d63a05
mschubert <mschu.dev@gmail.com>
2019-03-13 12:22:39 +0000

    add chr copy nums

--- data/aneup.r
+++ data/aneup.r
@@ -1,6 +1,16 @@
+io = import('io')
 sys = import('sys')
+tcga = import('data/tcga')
 
 args = sys$cmd$parse(
     opt('i', 'infile', 'file to read from', ''),
     opt('c', 'config', 'yaml', '../config.yaml'),
     opt('o', 'outfile', 'file to save to', 'aneup.RData'))
+
+cohorts = io$read_yaml(args$config)$cohorts
+
+chrs = lapply(cohorts, tcga$cna_chrs) %>%
+    lapply(reshape2::melt, value.name="copies") %>%
+    dplyr::bind_rows()
+
+save(chrs, file=args$outfile)
