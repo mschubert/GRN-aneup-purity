3ce5f5a0305267e56f4376ff2ae344cab5d2db8a
mschubert <mschu.dev@gmail.com>
2019-03-26 19:01:54 +0000

    simple meta merge

--- data/merge.r
+++ data/merge.r
@@ -1,3 +1,4 @@
+io = import('io')
 sys = import('sys')
 
 args = sys$cmd$parse(
@@ -5,3 +6,6 @@ args = sys$cmd$parse(
     opt('c', 'config', 'yaml', '../config.yaml'),
     arg('infiles', 'files to merge', arity='*',
         c("focal.RData", "aneup.RData", "purity.RData", "immune.RData")))
+
+dset = io$load(args$infiles)
+save(dset, file=args$outfile)
