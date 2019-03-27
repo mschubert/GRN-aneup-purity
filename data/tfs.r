b54aa35b6d5784c47a036b844668b2aaeb4433d0
mschubert <mschu.dev@gmail.com>
2019-03-27 13:00:44 +0000

    use HGNC names

--- data/tfs.r
+++ data/tfs.r
@@ -7,4 +7,6 @@ args = sys$cmd$parse(
     opt('o', 'outfile', 'file to save to', 'tfs.RData'))
 
 sets = enr$genes("ENCODE_and_ChEA_Consensus_TFs_from_ChIP-X")
+names(sets) = sub("_[^_]+$", "", names(sets))
+
 save(sets, file=args$outfile)
