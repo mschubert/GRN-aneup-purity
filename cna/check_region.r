e6c4f74ea38cea8c796fb5b737c39627a048c184
mschubert <mschu.dev@gmail.com>
2019-03-27 16:56:17 +0000

    split out possible/real links to util

--- cna/check_region.r
+++ cna/check_region.r
@@ -9,13 +9,14 @@ args = sys$cmd$parse(
     opt('c', 'cohort', 'chr', 'ACC'),
     opt('r', 'regions', 'RData', '../data/focal.RData'),
     opt('t', 'tfs', 'RData', '../data/tfs.RData'),
+    opt('m', 'method', 'aracne or genenet', 'aracne'),
     opt('n', 'network', 'RData', '../networks/aracne/ACC.RData'),
-    opt('o', 'outfile', '.RData', 'aracne/ACC.RData'))
+    opt('o', 'outfile', '.RData', 'focal_aracne/ACC.RData'))
 
+util = import(paste0("./", args$method))
 tfs = io$load(args$tfs)
-
 net = io$load(args$network)
-valid_genes = unique(c(net$Regulator, net$Target))
+valid_genes = util$valid_genes(net)
 
 # get gene sets of co-amplified segments (either focal CNA or aneuploidy)
 rset = io$load(args$regions)
@@ -25,23 +26,10 @@ cna_genes = rset$genes[keep] %>%
 
 # compare within-segment vs. outside of segment
 #   and TF targets vs non-TF targets (FET?)
-set2possible_links = function(genes, net) {
-    ntf = sum(genes %in% net$Regulator)
-    ntg = sum(genes %in% setdiff(net$Target, net$Regulator))
-    if (ntf > 0)
-        ntf * (ntf-1) + ntf * ntg
-    else
-        0
-}
-set2real_links = function(genes, net) {
-    net %>%
-        filter(Regulator %in% genes & Target %in% genes) %>%
-        nrow()
-}
 #TODO: also check for each segment individually
-psbl = c(seg = sum(sapply(cna_genes, set2possible_links, net=net)),
-         all = set2possible_links(valid_genes, net))
-real = c(seg = sum(sapply(cna_genes, set2real_links, net=net)),
+psbl = c(seg = sum(sapply(cna_genes, util$set2possible_links, net=net)),
+         all = util$set2possible_links(valid_genes, net))
+real = c(seg = sum(sapply(cna_genes, util$set2real_links, net=net)),
          all = nrow(net))
 links = rbind(real, psbl) # so estimate is OR segments over rest
 links[,2] = links[,2] - links[,1] # adjust all to outside of segment
