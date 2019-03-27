2564746a0089f2233981af1dfdbcbd73bc4f7716
mschubert <mschu.dev@gmail.com>
2019-03-27 16:46:19 +0000

    refactor

--- cna/check_region.r
+++ cna/check_region.r
@@ -6,31 +6,23 @@ gset = import('data/genesets')
 tcga = import('data/tcga')
 
 args = sys$cmd$parse(
-    opt('m', 'meta', 'RData', '../data/meta.RData'),
-    opt('t', 'tfs', 'RData', '../data/tfs.RData'),
     opt('c', 'cohort', 'chr', 'ACC'),
-#    opt('r', 'ranges', 'RData', '../'),
+    opt('r', 'regions', 'RData', '../data/focal.RData'),
+    opt('t', 'tfs', 'RData', '../data/tfs.RData'),
     opt('n', 'network', 'RData', '../networks/aracne/ACC.RData'),
-    opt('o', 'outfile', '.RData', 'ACC.RData'))
+    opt('o', 'outfile', '.RData', 'aracne/ACC.RData'))
 
-meta = io$load(args$meta)
 tfs = io$load(args$tfs)
+
 net = io$load(args$network)
-
-focal = meta$focal %>%
-    filter(tcga$barcode2study(Sample) == args$cohort)
-
 net_all = unique(c(net$Regulator, net$Target))
 net_tfs = unique(net$Regulator)
 net_tgs = setdiff(net_all, net_tfs)
 
-# quantify the copy number of every tissue-specific segment
-#   divide this by purity to get the cancer copies
-cnas = narray::construct(copies ~ Sample + name, data=focal)
-cna_genes = ungroup(focal) %>%
-    select(name, gene) %>%
-    filter(!duplicated(name)) %$%
-    setNames(gene, name) %>%
+# get gene sets of co-amplified segments (either focal CNA or aneuploidy)
+rset = io$load(args$regions)
+keep = rownames(rset$copies[[args$cohort]])
+cna_genes = rset$genes[keep] %>%
     gset$filter(min=2, max=Inf, valid=net_all)
 
 # compare within-segment vs. outside of segment
@@ -39,7 +31,6 @@ set2possible_links = function(genes) {
     ntf = sum(genes %in% net_tfs)
     ntg = sum(genes %in% net_tgs)
     if (ntf > 0)
-        #ntf-1 + ntg
         ntf * (ntf-1) + ntf * ntg
     else
         0
@@ -49,14 +40,18 @@ set2real_links = function(genes, net) {
         filter(Regulator %in% genes & Target %in% genes) %>%
         nrow()
 }
-possible = c(within = sum(sapply(cna_genes, set2possible_links)),
-             outside = set2possible_links(net_all) - within_segs)
-real = c(within = sum(sapply(cna_genes, set2real_links, net=net)),
-         outside = nrow(net))
-rbind(real, possible=possible-real) %>%
-    fisher.test() %>%
-    broom::tidy()
+#TODO: also check for each segment individually
+psbl = c(seg = sum(sapply(cna_genes, set2possible_links)),
+         all = set2possible_links(net_all))
+real = c(seg = sum(sapply(cna_genes, set2real_links, net=net)),
+         all = nrow(net))
+links = rbind(real, psbl) # so estimate is OR segments over rest
+links[,2] = links[,2] - links[,1] # adjust all to outside of segment
+links[2,] = links[2,] - links[1,] # adjust possible to "not chosen"
+res = broom::tidy(fisher.test(links))
+
+res
+save(res, file=args$outfile)
 
 # plot total number of amp segments (/genes?) per cancer type
-
 # plot enrichment of amp/del in segments per method
