a42c3fd88b04e9f911caf751091f6a0ca7fa72a2
mschubert <mschu.dev@gmail.com>
2019-04-16 10:29:13 +0000

    adjust to integrated util

--- set_enrichment/check_set_enrichment.r
+++ set_enrichment/check_set_enrichment.r
@@ -14,10 +14,9 @@ args = sys$cmd$parse(
 
 util = import(paste0("./", args$method))
 net = io$load(args$network)
-valid_genes = util$valid_genes(net)
 
-if (method == "aracne") { # for now, only one with knowledge what is TF
-    valid_genes = function(net) unique(c(net$Regulator, net$Target))
+if (args$method == "aracne") { # for now, only one with knowledge what is TF
+    valid_genes = unique(c(net$Regulator, net$Target))
     set2possible_links = function(genes, net) {
         ntf = sum(genes %in% net$Regulator)
         ntg = sum(genes %in% setdiff(net$Target, net$Regulator))
@@ -32,8 +31,7 @@ if (method == "aracne") { # for now, only one with knowledge what is TF
             nrow()
     }
 } else {
-    valid_genes = function(net) unique(c(as.character(net$node1),
-                                         as.character(net$node2)))
+    valid_genes = unique(c(as.character(net$node1), as.character(net$node2)))
     set2possible_links = function(genes, net) {
         ng = length(genes)
         0.5 * (ng^2 - ng)
@@ -57,9 +55,9 @@ cna_genes = gset$filter(cna_genes, min=2, max=Inf, valid=valid_genes)
 # compare within-segment vs. outside of segment
 #   and TF targets vs non-TF targets (FET?)
 #TODO: also check for each segment individually
-psbl = c(seg = sum(sapply(cna_genes, util$set2possible_links, net=net)),
-         all = util$set2possible_links(valid_genes, net))
-real = c(seg = sum(sapply(cna_genes, util$set2real_links, net=net)),
+psbl = c(seg = sum(sapply(cna_genes, set2possible_links, net=net)),
+         all = set2possible_links(valid_genes, net))
+real = c(seg = sum(sapply(cna_genes, set2real_links, net=net)),
          all = nrow(net))
 links = rbind(real, psbl) # so estimate is OR segments over rest
 links[,2] = links[,2] - links[,1] # adjust all to outside of segment
