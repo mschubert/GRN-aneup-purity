164a27e8670acf599e849440c1e13c581737a335
mschubert <mschu.dev@gmail.com>
2019-04-16 12:03:33 +0000

    multiple net cutoffs

--- set_enrichment/check_set_enrichment.r
+++ set_enrichment/check_set_enrichment.r
@@ -12,7 +12,6 @@ args = sys$cmd$parse(
     opt('n', 'network', 'RData', '../networks/aracne/naive/ACC.RData'),
     opt('o', 'outfile', '.RData', 'focal_aracne/naive/ACC.RData'))
 
-util = import(paste0("./", args$method))
 net = io$load(args$network)
 
 if (args$method == "aracne") { # for now, only one with knowledge what is TF
@@ -44,28 +43,38 @@ if (args$cohort %in% names(rset$sets)) { # separate sets for each cohort
 cna_genes = gset$filter(cna_genes, min=2, max=Inf, valid=valid_genes)
 
 # compare within-segment vs. outside of segment
-#   and TF targets vs non-TF targets (FET?)
-do_test = function(seg_real, all_real, seg_psbl, all_psbl, ...) {
-    links = rbind(c(seg_real, all_real), c(seg_psbl, all_psbl))
-    links[,2] = links[,2] - links[,1] # adjust all to outside of segment
-    links[2,] = links[2,] - links[1,] # adjust possible to "not chosen"
-    broom::tidy(fisher.test(links)) %>%
-        select(-method, -alternative) %>%
-        mutate(links = list(links))
+#   and TF targets vs non-TF targets
+calc_net = function(net, co) {
+    net2 = head(net, co)
+    do_test = function(seg_real, all_real, seg_psbl, all_psbl, ...) {
+        links = rbind(c(seg_real, all_real), c(seg_psbl, all_psbl))
+        links[,2] = links[,2] - links[,1] # adjust all to outside of segment
+        links[2,] = links[2,] - links[1,] # adjust possible to "not chosen"
+        broom::tidy(fisher.test(links)) %>%
+            select(-method, -alternative) %>%
+            mutate(links = list(links))
+    }
+    make_counts = function(fun) {
+        segs = sapply(cna_genes, fun, net=net2)
+        c(all=sum(segs), segs)
+    }
+    res = data.frame(seg_psbl = make_counts(set2possible_links), # filtered network
+                     seg_real = make_counts(set2real_links),
+                     all_psbl = set2possible_links(valid_genes, net), # full network
+                     all_real = nrow(net)) %>%
+        tibble::rownames_to_column("seg_id") %>%
+        as_tibble() %>%
+        filter(seg_psbl > 0) %>%
+        mutate(fet = purrr::pmap(., do_test)) %>%
+        tidyr::unnest()
 }
-make_counts = function(fun, keep=FALSE) {
-    segs = sapply(cna_genes, fun, net=net)
-    c(all=sum(segs), segs)
-}
-res = data.frame(seg_psbl = make_counts(set2possible_links),
-                 seg_real = make_counts(set2real_links),
-                 all_psbl = set2possible_links(valid_genes, net),
-                 all_real = nrow(net)) %>%
-    tibble::rownames_to_column("seg_id") %>%
-    as_tibble() %>%
-    filter(seg_psbl > 0) %>%
-    mutate(fet = purrr::pmap(., do_test)) %>%
-    tidyr::unnest()
+cutoff = nrow(net)
+if (grepl("\\.wrap", args$method))
+    cutoff = exp(seq(log(1e3), log(1e6), length.out=20))
+all_cuts = lapply(cutoff, calc_net, net=net) %>%
+    setNames(cutoff) %>%
+    bind_rows(.id="edges") %>%
+    mutate(edges = as.integer(edges))
 
-res
-save(res, file=args$outfile)
+all_cuts
+save(all_cuts, file=args$outfile)
