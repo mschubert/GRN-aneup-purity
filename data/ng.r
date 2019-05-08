45aac92b6d3d4b24b1ecfbc0bb6ba74525f2a4df
mschubert <mschu.dev@gmail.com>
2019-05-08 12:05:20 +0200

    also count number of binding interactions

--- data/ng.r
+++ data/ng.r
@@ -5,18 +5,22 @@ sys = import('sys')
 count_cohort = function(fname) {
     expr = io$load(fname)
     data.frame(ng = nrow(expr),
-         ntf = length(intersect(rownames(expr), tfs)),
-         nsmp = ncol(expr)
+         ntf = length(intersect(rownames(expr), tfa)),
+         nsmp = ncol(expr),
+         nint = nrow(filter(tftg, ind %in% rownames(expr) & values %in% rownames(expr)))
     )
 }
 
 args = sys$cmd$parse(
     opt('a', 'annot', 'TF annotation RData', 'tf_annot.RData'),
+    opt('b', 'binding', 'TF binding RData', 'tf_binding.RData'),
     opt('o', 'outfile', 'file to save df to', 'ng.RData'),
     arg('expr', 'expr RData', arity='*',
         list.files("expr_naive", recursive=TRUE, full.names=TRUE)))
 
-tfs = io$load(args$annot)
+tfa = io$load(args$annot)
+tfb = io$load(args$binding)
+tftg = rev(stack(tfb))
 
 ng = do.call(rbind, strsplit(tools::file_path_sans_ext(args$expr), "[_/]")) %>%
     `[`(,c(ncol(.):1)) %>%
