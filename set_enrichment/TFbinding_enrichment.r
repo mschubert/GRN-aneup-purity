bc788eea287384dec5a26fdabd1cd846a5d51cc2
mschubert <mschu.dev@gmail.com>
2019-05-09 11:51:51 +0200

    fix network size axis

--- set_enrichment/TFbinding_enrichment.r
+++ set_enrichment/TFbinding_enrichment.r
@@ -8,7 +8,7 @@ check_hits = function(net, real) {
     re = c(paste(as.character(net[[1]]), as.character(net[[2]])),
            paste(as.character(net[[2]]), as.character(net[[1]]))) %in%
         paste(as.character(real[[1]]), as.character(real[[2]])) + 0
-    roc = data.frame(TP = c(0,cumsum(re)), size = c(0,seq_len(re)))
+    roc = data.frame(TP = c(0,cumsum(re)), size = c(0,seq_along(re)))
     roc2 = roc[re != 0 | c(1,re[-c(1,2)], 1) != 0,]
 }
 
@@ -39,7 +39,7 @@ hits = do.call(rbind, strsplit(tools::file_path_sans_ext(args$net), "/")) %>%
 
 save(hits, ng, file=args$outfile)
 
-p = ggplot(hits, aes(x=FP, y=TP, color=method)) +
+p = ggplot(hits, aes(x=size, y=TP, color=method)) +
     geom_abline(data=ng, aes(slope=slope, intercept=0), color="grey", linetype="dashed") +
     geom_line() +
     facet_grid(expr ~ cohort) +
